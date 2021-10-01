9dc133495a07c0eaa874b6abff597c846d434c93
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-10-01 11:58:38 +0200

    Add main architecture of files

---
+++ src/app/components/infirmieres/infirmieres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-infirmieres',
+  templateUrl: './infirmieres.component.html',
+  styleUrls: ['./infirmieres.component.scss']
+})
+export class InfirmieresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
